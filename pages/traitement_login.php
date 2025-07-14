b46cc9ebef381b93b7c77604ed6a9276c597fa54
funaki302 <rafanomezantsoafunakilive@gmail.com>
2025-07-14 12:38:50 +0300

    Funaki

--- pages/traitement_login.php
+++ pages/traitement_login.php
@@ -11,9 +11,8 @@ $bdd = dbconnect();
     if ($email && $mdp) {
         $query = "SELECT * FROM membre WHERE email = '$email'";
         $result = mysqli_query($bdd, $query);
-        if ($result && mysqli_num_rows($result) > 0) {
+        if ( mysqli_num_rows($result) > 0) {
             $user = mysqli_fetch_assoc($result);
-            if (password_verify($mdp, $user['mdp'])) {
                 $_SESSION['user_id'] = $user['id_membre'];
                 $_SESSION['user_name'] = $user['nom'];
                 header("Location: objects.php");
@@ -26,9 +25,5 @@ $bdd = dbconnect();
             header("Location: index.php?message=" . urlencode("Email ou mot de passe incorrect."));
             exit;
         }
-    } else {
-        header("Location: index.php?message=" . urlencode("Veuillez remplir tous les champs."));
-        exit;
-    }
 
 ?>
